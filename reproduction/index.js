e5c05176643fd9e92dc237cf3303dc8f9652918a
Kevan Lu <kevan.lu@mail.mcgill.ca>
2014-11-12 15:25:13 -0500

    Added files for Week 1 of Repro

--- reproduction/index.js
+++ reproduction/index.js
@@ -1,4 +1,16 @@
 var tipuesearch = {"pages": [
+	{"title": "Female Pelvis Anatomy Lab", "text": "", "tags": "", "loc": "http://www.schulichnotes.com/reproduction/2014/11/14/female-pelvis-anatomy-lab.html"},
+	{"title": "Pharmacology of Contraception", "text": "", "tags": "", "loc": "http://www.schulichnotes.com/reproduction/2014/11/14/pharmacology-contraception.html"},
+	{"title": "The HPO Axis and the Menstrual Cycle", "text": "", "tags": "", "loc": "http://www.schulichnotes.com/reproduction/2014/11/13/hpo-axis-menstrual-cycle.html"},
+	{"title": "Introduction to the Gynecological Clinical Exam", "text": "", "tags": "", "loc": "http://www.schulichnotes.com/reproduction/2014/11/13/introduction-gynecological-clinical-exam.html"},
+	{"title": "Female Reproductive Physiology", "text": "FEED FORWARD RESPONSE = positive feedback but with a TIME DELAY.", "tags": "", "loc": "http://www.schulichnotes.com/reproduction/2014/11/12/female-reproductive-physiology.html"},
+	{"title": "Male Reproductive Physiology", "text": "", "tags": "", "loc": "http://www.schulichnotes.com/reproduction/2014/11/12/male-reproductive-physiology.html"},
+	{"title": "Histology &amp; Pathology of Female &amp; Male Reproductive Tracts", "text": "", "tags": "", "loc": "http://www.schulichnotes.com/reproduction/2014/11/11/histology-pathology-female-male-reproductive-tract.html"},
+	{"title": "Functional Sexual Anatomy", "text": "", "tags": "", "loc": "http://www.schulichnotes.com/reproduction/2014/11/11/functional-sexual-anatomy.html"},
+	{"title": "Anatomy of the Perineum", "text": "", "tags": "", "loc": "http://www.schulichnotes.com/reproduction/2014/11/11/anatomy-perineum.html"},
+	{"title": "Anatomy of the Female Reproductive Tract", "text": "", "tags": "", "loc": "http://www.schulichnotes.com/reproduction/2014/11/10/anatomy-female-reproductive-tract.html"},
+	{"title": "Embryology of the Female Reproductive Tract", "text": "", "tags": "", "loc": "http://www.schulichnotes.com/reproduction/2014/11/10/embryology-female-reproductive-tract.html"},
+	{"title": "Introduction to Reproduction", "text": "", "tags": "", "loc": "http://www.schulichnotes.com/reproduction/2014/11/10/intro-reproduction.html"},
 	{"title": "Reproduction", "text": "Course notes for Reproduction", "tags": "", "loc": "http://www.schulichnotes.com/reproduction"}
 ]};
 
